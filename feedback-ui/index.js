5ef9ec3c0e1d59fbea52c2647ea8593972d83300
Yash Pathak <90565364+Its-Yash@users.noreply.github.com>
2023-03-17 22:20:35 +0530

    Add files via upload

---
+++ feedback-ui/index.js
@@ -0,0 +1,49 @@
+const ratingEls = document.querySelectorAll(".rating");
+const btnEl = document.getElementById("btn");
+const containerEl = document.getElementById("container")
+
+let selectedRating = "";
+
+ratingEls.forEach((ratingEl) =>{
+    ratingEl.addEventListener("click", (event)=> {
+        removeActive();
+        selectedRating = event.target.innerText || event.target.parentNode.innerText
+        event.target.classList.add("active")
+        event.target.parentNode.classList.add("active")
+    })
+})
+
+btnEl.addEventListener("click", ()=>{
+    if(selectedRating !== ""){
+        containerEl.innerHTML = `
+        <strong>Thank you!</strong>
+        <br>
+        <br>
+        <strong>Feedback: ${selectedRating}</strong>
+        <p>We'll use your feedback to improve our coustomer support</p>
+        `
+    }
+})
+
+function removeActive(){
+    ratingEls.forEach((ratingEl) =>{
+        ratingEl.classList.remove("active")
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
